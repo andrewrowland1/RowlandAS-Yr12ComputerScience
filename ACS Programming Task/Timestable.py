5f788a63c6e774264f9bd59cfe691c385f5ed067
andrewrowland1 <34370637+andrewrowland1@users.noreply.github.com>
2019-09-06 11:49:01 +0100

    Update Timestable.py

--- ACS Programming Task/Timestable.py
+++ ACS Programming Task/Timestable.py
@@ -1,22 +1,21 @@
 #program that asks the user for a number between 1 and 10 and then outputs the times table for that number
-i_Number = int(input("Please input a number between 1 and 10")
-#Check if number is between 1 
-while (i_Number < 0 or i_Number > 10:)
-                    i_Number = int(input("You did not enter a number between 1 and 10, please enter another number between 1 and 10")
+i_Number = int(input("Please input a number between 1 and 10"))
+#Check if number is between 1 and 10
+
+while i_Number < 1 or i_Number > 10:
+    i_Number = int(input("Please input a number between 1 and 10"))
+
+#program will times the number by the counter. after each loop the counter increases by 1 until the counter is above 12. then it will terminate
+for counter in range (1,13):
+    print(i_Number * counter)
+    counter = counter + 1
+    
 
 
-print(i_Number * 1)
-print(i_Number * 2)
-print(i_Number * 3)
-print(i_Number * 4)
-print(i_Number * 5)
-print(i_Number * 6)
-print(i_Number * 7)
-print(i_Number * 8)
-print(i_Number * 9)
-print(i_Number * 10)
-print(i_Number * 11)
-print(i_Number * 12)
+
+
+
+
             
 
                
