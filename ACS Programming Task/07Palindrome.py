dc02da45fa3022a4dda1364cdbd2a85e0e640225
andrewrowland1 <34370637+andrewrowland1@users.noreply.github.com>
2019-09-10 18:19:51 +0100

    10/09/19

--- ACS Programming Task/07Palindrome.py
+++ ACS Programming Task/07Palindrome.py
@@ -1,2 +1,9 @@
 #program that takes a string and reverses it eg 'hello' goes to 'olleh'
 text = input(str("Please input a string"))
+print(text)
+def reverse(text):
+    str = ""
+    for reversetext in text:
+        str = reversetext + str
+    return str
+print(reverse(text))
