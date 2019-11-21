5d7d50069ac00f3528ac8c7a1990138cad9a0ecd
andrewrowland1 <34370637+andrewrowland1@users.noreply.github.com>
2019-11-21 12:25:12 +0000

    21/11/19

--- ACS Programming Task/Pygame/4 bouncing balls on 4 screens.py
+++ ACS Programming Task/Pygame/4 bouncing balls on 4 screens.py
@@ -7,25 +7,26 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 WHITE = (0,0,0)
-RANDOM = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+RANDOM1 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+RANDOM2 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+RANDOM3 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+RANDOM4 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 #Initialise Pygame
 pygame.init()
 #Set the height and width of the screen
-screen_width = 700
-screen_height = 400
+screen_width = 500
+screen_height = 500
 screen = pygame.display.set_mode([screen_width,screen_height])
 #Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
 #define lists
 theBall = []
-newBall = []
 colourList = [RED,GREEN,BLUE,WHITE]
-numNew = 0
 #Define the class Ball
 class Ball():
     #Constructor function to define initial state of a ball object
-    def __init__(self,x,y,col,x_speed,y_speed,):
+    def __init__(self,x,y,col,x_speed,y_speed,minX,minY,maxX,maxY):
         #---Class Attributes ---
         #Ball position
         self.x = x
@@ -40,15 +41,20 @@ class Ball():
 
         #Ball colour
         self.color = col
+        #borders
+        self.minX = minX
+        self.minY = minY
+        self.maxX = maxX
+        self.maxY= maxY
     #--- Class Methods ---
     #Defines the ball's movement
     def move(self):
         self.x += self.change_x
         self.y += self.change_y
-        if self.x >= screen_width or self.x <= 0:
+        if self.x >= self.maxX or self.x <= self.minX:
             self.change_x *= -1
         #endif
-        if self.y >= screen_height or self.y <= 0:
+        if self.y >= self.maxY or self.y <= self.minY:
             self.change_y *= -1
         #endif
 
@@ -59,43 +65,33 @@ class Ball():
 done = False
 
 #Create an object using the ball class
-for counter in range(0,1):
-    
-    X = random.randint(1,700)
-    Y = random.randint(1,400)
-    SIZE = random.randint(5,25)
-    Colour = random.choice(colourList)
-    xSpeed = random.randint(-10,10)
-    ySpeed = random.randint(-10,10)
-    theBall.append(Ball(X,Y,RANDOM,xSpeed,ySpeed))
+theBall1 = Ball(random.randint(0,250),random.randint(0,250),RANDOM1,10,-10,0,0,250,250)
+theBall2 = Ball(random.randint(0,250),random.randint(250,500),RANDOM2,10,10,0,250,250,500)
+theBall3 = Ball(random.randint(250,500),random.randint(0,250),RANDOM3,10,-10,250,0,500,250)
+theBall4 = Ball(random.randint(250,500),random.randint(250,500),RANDOM4,10,10,250,250,500,500)
 
+                
 
 #Game loop
 while not (done):
-    RANDOM = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+    
     #Clear the screen
     screen.fill(WHITE)
     #Draw the ball on the screen and then move it
-    for counter in range(0,1 + numNew):
-        theBall[counter].draw(screen)
-        theBall[counter].move()
+    
+    theBall1.draw(screen)
+    theBall1.move()
+    theBall2.draw(screen)
+    theBall2.move()
+    theBall3.draw(screen)
+    theBall3.move()
+    theBall4.draw(screen)
+    theBall4.move()
         
-    #Spacebar to spawn new ball
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                for counter in range(0,10000):
-                    RANDOM = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-                    theBall.append(Ball(random.randint(1,700),random.randint(1,400),RANDOM,random.randint(-10,10),random.randint(-10,10)))
-                    numNew = numNew + 1
-            if event.key == pygame.K_p:
-                done = True
+    
                 
     #Limit to 60 fps
     clock.tick(60)
     #Go ahead and update the screen with what we've drawn
     pygame.display.flip()
 pygame.quit()
-    
